51ac6de90eb6fc2e4bcf80d9bd749a3fd99c6d8b
Oleg Smarandi <olegsmarandi@gmail.com>
2021-05-08 13:29:28 +0300

    v 0.2

--- EasyNaming.py
+++ EasyNaming.py
@@ -15,35 +15,38 @@ class MockupsProperties:
 		self.initial_f_n = initial_f_n
 		self.export_f_n = export_f_n
 		self.n_mockups = 0
+		self.mockup_files = ["Техническое задание.pdf", "preview.png"]
+		if color_scheme == "RGB":
+			self.export_files = ["72ppi.jpg", "300ppi.jpg"]
+			self.initial_files = ["Illustrator 2020.ai", "Illustrator 10.ai", "Project.eps"]
+		else:
+			self.initial_files = ["Illustrator 2020.ai", "Illustrator 10.ai", "Project.eps"]
+			self.export_files = ["72ppi lzw.tiff", "300ppi lzw.tiff"]
 
 
 class NamingHelper:
 	def __init__(self, pp: ProjectProperties):
 		self.pp = pp
-		self.initial_files = []
-		self.export_files = []
 
 	def create_mockups(self, mp: MockupsProperties):
 		main_folder_name = os.path.join(self.pp.project_folder, self.pp.city_name, mp.color_scheme)
 		os.makedirs(main_folder_name)
 		for i in range(mp.n_mockups):
-			mockup_path_initial = os.path.join(main_folder_name, f"mockup_{i+1}", mp.initial_f_n)
-			mockup_path_export = os.path.join(main_folder_name, f"mockup_{i+1}", mp.export_f_n)
-			os.makedirs(mockup_path_initial)
-			self.trace(mockup_path_initial)
-			os.makedirs(mockup_path_export)
-			self.trace(mockup_path_export)
+			mockup_path = os.path.join(main_folder_name, f"mockup_{i+1}")
+			self.fill_mockup(mockup_path, mp.initial_f_n, mp.initial_files)
+			self.fill_mockup(mockup_path, mp.export_f_n, mp.export_files)
+			self.create_empty_files(mockup_path, mp.mockup_files)
 
-	def create_empty_files_initial(self, mockup_path_initial):
-		for file in self.initial_files:
-			file = os.path.join(mockup_path_initial, file)
-			with open(file, 'w') as file:
-				pass
+	def fill_mockup(self, mockup_path, folder_n, files):
+		mockup_path_folder_n = os.path.join(mockup_path, folder_n)
+		os.makedirs(mockup_path_folder_n)
+		self.create_empty_files(mockup_path_folder_n, files)
+		self.trace(mockup_path_folder_n)
 
-	def create_empty_files_export(self, mockup_path_export):
-		for file in self.export_files:
-			file = os.path.join(mockup_path_export, file)
-			with open(file, 'w') as file:
+	def create_empty_files(self, path, files):
+		for file in files:
+			self.trace(os.path.join(path, file))
+			with open(os.path.join(path, file), 'w') as fp:
 				pass
 
 	def trace(self, path):
